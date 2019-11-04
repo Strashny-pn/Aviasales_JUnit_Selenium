a8eaf3470d4577f4dc3725fa3d2e7a0ed567c772
Strashny-pn <Pasik_76@mail.ru>
2019-11-05 05:10:36 +0600

    Add next step

--- Strashny_PN_Aviasales/src/test/java/junit/AviaSalesBasicTest.java
+++ Strashny_PN_Aviasales/src/test/java/junit/AviaSalesBasicTest.java
@@ -24,15 +24,13 @@ public class AviaSalesBasicTest {
         invisibleElement("//div[@class='custom-radio__caption' and text()='Эконом']");
         clickWebElementByXpath("//input[@id='clicktripz']/..");
         clickElementByJsByXpath("//span[text()='Найти билеты']/..");
-        System.out.println(Webdriver.getWebdriver().getCurrentUrl());
         waitWebElement("//div[contains(@class,'loader__stripes --animation-finished')]");
         clickWebElementByXpath("//label[@class='checkboxes-list__label' and text()='Все']");
         clickWebElementByXpath("//label[@class='checkboxes-list__label' and text()='Без пересадок']");
 
-
-
-
-
+        clickWebElementByXpath("//div[@class='filter__header' and text()='Багаж']");
+        clickWebElementByXpath("//div[@class='filter__header' and text()='Багаж']/..//label[@class='checkboxes-list__label' and text()='Все']");
+        clickWebElementByXpath("//div[@class='filter__header' and text()='Багаж']/..//label[@class='checkboxes-list__label' and text()='Багаж и ручная кладь']");
 
 
         try {
